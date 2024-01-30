fa837bbc948f2b8247166f19ac62e03b23299d3a
Zander Sparrow <alexsilverhawk@gmail.com>
2024-01-30 12:59:41 -0800

    Add MaterialUI and form components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lightBlue, pink } from '@mui/material/colors';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Landing from './pages/Landing';
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: lightBlue,
+    secondary: pink,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Landing />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main className="App">
+        <header className="App-container">
+          <Landing />
+        </header>
+      </main>
+    </ThemeProvider>
+    
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,51 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
 
 const Login = ({onLogin}) => {
   const [user, setUser] = React.useState();
+  const [errors, setErrors] = React.useState({});
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        required
+        error={errors.email ? true : false}
+        label="Email"
+        type="email"
+        variant="standard"
+        onChange={(event) => {
+          setUser(event.target.value);
+        }}
+      />
+
+      <TextField
+          required
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="standard"
+        />
+
+      <Button 
+        variant="contained" 
+        size="large" 
+        color="primary"
+        onClick={() => console.log(user)}
+      >
+        Login
+      </Button>
+    </Box>
+  );
 
   return (
     <div>
